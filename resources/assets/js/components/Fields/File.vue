69608f1d7bc38590827be309838bb279978a3f6d
daniels.grietins <daniels.grietins@netcore.lv>
2018-09-04 14:19:01 +0300

    File uploads per language

--- resources/assets/js/components/Fields/File.vue
+++ resources/assets/js/components/Fields/File.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="form-group">
-        <label for="">
-            {{ input.label }}
+        <label for="">{{ input.label }}
+            <span class="badge badge-primary"
+                  v-if="input.isTranslatable">
+                {{ language }}
+            </span>
         </label>
         <br>
-        <a :href="input.url" v-if="input.url" target="_blank">
-            {{ input.file_name }} ({{ input.file_size }} kb)
-        </a>
-        <input type="file" :name="input.name" class="form-control">
+        <div v-if="input.isTranslatable">
+            <a v-for="item in input.translatedAttributes" :href="item.url" v-if="item.url" v-show="language === item.iso_code" target="_blank">
+                {{ item.file_name }} ({{ item.file_size }} kb)
+            </a>
+            <input type="file" :name="item.name" class="form-control" v-for="item in input.translatedAttributes"
+                   v-show="language === item.iso_code">
+        </div>
+        <div v-else>
+            <a :href="input.url" v-if="input.url" target="_blank">
+                {{ input.file_name }} ({{ input.file_size }} kb)
+            </a>
+            <input type="file" :name="input.name" class="form-control">
+        </div>
     </div>
 </template>
 
