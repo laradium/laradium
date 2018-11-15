f8c6af830068e881d128068440f01d12270be475
Daniels Grietins <daniels.grietins@netcore.lv>
2018-11-15 21:45:43 +0200

    Added svg icon select field

---
+++ resources/assets/js/components/Fields/SvgIcon.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label for="">{{ field.label }}
+                <span class="badge badge-primary"
+                      v-if="field.config.is_translatable">
+					{{ language }}
+				</span>
+            </label>
+
+            <select :name="field.name" class="form-control" v-bind="attributes">
+                <option
+                        :value="file_name"
+                        :selected="file_name === field.value"
+                        v-for="(name, file_name) in field.options">
+                    {{ name }}
+                </option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['field', 'language', 'item'],
+
+        computed: {
+            attributes() {
+                return this.field.attr;
+            }
+        }
+    }
+</script>
