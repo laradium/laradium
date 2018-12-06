adfe142ba1cf6da93671c0d49f787f2ae9cfebcf
Dmitrijs Mihailovs <dmitrijs.mihailovs@netcore.lv>
2018-12-06 11:42:16 +0200

    Add fields to boolean

--- src/Base/Fields/Boolean.php
+++ src/Base/Fields/Boolean.php
@@ -3,9 +3,46 @@
 namespace Laradium\Laradium\Base\Fields;
 
 use Laradium\Laradium\Base\Field;
+use Laradium\Laradium\Base\FieldSet;
 
 class Boolean extends Field
 {
+
+    /**
+     * @var FieldSet
+     */
+    private $fieldSet;
+
+    /**
+     * @var array
+     */
+    private $templateData = [];
+
+    /**
+     * BelongsToMany constructor.
+     * @param $parameters
+     * @param Model $model
+     */
+    public function __construct($parameters, $model)
+    {
+        parent::__construct($parameters, $model);
+
+        $this->fieldSet = new FieldSet;
+    }
+
+    /**
+     * @param array $attributes
+     * @return $this|Field
+     */
+    public function build($attributes = [])
+    {
+        parent::build($attributes);
+
+        $this->templateData = $this->getTemplateData();
+        $this->validationRules($this->templateData['validation_rules']);
+
+        return $this;
+    }
 
     /**
      * @return array
@@ -14,7 +51,50 @@ class Boolean extends Field
     {
         $data = parent::formattedResponse();
         $data['checked'] = $this->getValue() == 1;
+        $data['fields'] = $this->templateData['fields'];
 
         return $data;
+    }
+
+    /**
+     * @param $closure
+     * @return $this
+     */
+    public function fields($closure)
+    {
+        $fieldSet = $this->fieldSet;
+        $fieldSet->model($this->getModel());
+        $closure($fieldSet);
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    private function getTemplateData()
+    {
+        $fields = [];
+        $validationRules = [];
+
+        foreach ($this->fieldSet->fields as $temporaryField) {
+            $field = clone $temporaryField;
+            $attributes = $this->getAttributes();
+            $lastAttribute = array_pop($attributes);
+
+            $field->model($this->getModel())
+                ->build($attributes);
+
+            if ($field->getRules()) {
+                $validationRules[$field->getValidationKey()] = $field->getRules();
+            }
+
+            $fields[] = $field->formattedResponse();
+        }
+
+        return [
+            'fields'           => $fields,
+            'validation_rules' => $validationRules
+        ];
     }
 }
