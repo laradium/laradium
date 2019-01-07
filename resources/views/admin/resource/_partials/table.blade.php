877cf9a2e16e200f74689491e766c8c5b10e20dd
Dmitrijs Mihailovs <dmitrijs.mihailovs@netcore.lv>
2019-01-07 16:36:28 +0200

    Datatable editable translations, modified fields

--- resources/views/admin/resource/_partials/table.blade.php
+++ resources/views/admin/resource/_partials/table.blade.php
@@ -1,10 +1,10 @@
 <div class="table-wrapper">
     <div class="table-responsive">
-        <table class="resource-datatable table table-bordered" {{ isset( $dataUrl ) ? 'data-url="' . $dataUrl . '"' : '' }}>
+        <table class="resource-datatable table table-bordered" {{ isset($dataUrl) ? 'data-url=' . $dataUrl . '' : '' }}>
             <thead>
             <tr>
                 @foreach($table->columns() as $column)
-                    <th {{ $column['column'] == 'action' ? 'style="width: 150px"' : '' }}>{{ ucfirst(str_replace('_', ' ', $column['name'])) }}</th>
+                    <th {{ $column['column'] === 'action' ? 'style="width: 150px"' : '' }}>{{ ucfirst(str_replace('_', ' ', $column['name'])) }}</th>
                 @endforeach
                 @if(!$table->columns()->where('column', 'action')->first())
                     <th style="width: 150px">
