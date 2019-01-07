dfcd15549cd48ab75965d1748ba14080401a09af
Dmitrijs Mihailovs <dmitrijs.mihailovs@netcore.lv>
2019-01-07 19:22:07 +0200

    Fix datatable tabs data url

--- resources/views/admin/resource/_partials/table.blade.php
+++ resources/views/admin/resource/_partials/table.blade.php
@@ -1,6 +1,6 @@
 <div class="table-wrapper">
     <div class="table-responsive">
-        <table class="resource-datatable table table-bordered" {{ isset( $dataUrl ) ? 'data-url="' . $dataUrl . '"' : '' }}>
+        <table class="resource-datatable table table-bordered" {{ isset($dataUrl) ? 'data-url=' . $dataUrl . '' : '' }}>
             <thead>
             <tr>
                 @foreach($table->columns() as $column)
