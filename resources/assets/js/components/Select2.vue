cd99238de2099f46b8073a956d6c4670a879618f
speekadievs <arturs.lukjanenoks@netcore.lv>
2019-05-21 11:52:06 +0300

    Add ajax support for select2 field

--- resources/assets/js/components/Select2.vue
+++ resources/assets/js/components/Select2.vue
@@ -5,8 +5,12 @@
 </template>
 
 <script>
+    import _ from 'lodash';
+    import axios from 'axios';
+
     export default {
         props: ['options', 'value', 'config', 'multiple'],
+
         data() {
             return {
                 defaultConfig: {
@@ -21,9 +25,26 @@
                 }
             };
         },
-        mounted: function () {
+
+        async mounted() {
+            Object.assign(this.defaultConfig, this.config);
+
             let config = this.config ? this.config : this.defaultConfig;
             let select = $(this.$el);
+
+            if (this.config.source) {
+                let selected = await this.findSelected();
+
+                if (selected) {
+                    this.appendOption(selected);
+                }
+
+                config.ajax = {
+                    url: this.config.source,
+                    data: (params) => this.processQuery(params),
+                    processResults: (data) => this.processResults(data)
+                }
+            }
 
             select
                 .val(this.value)
@@ -32,15 +53,78 @@
                     this.$emit('input', select.val())
                 });
         },
+
+        methods: {
+            async findSelected() {
+                let response = await axios.get(this.config.source, this.config.query_params);
+
+                let data = response.data;
+                if (this.config.data_property) {
+                    data = response.data[this.config.data_property];
+                }
+
+                return _.find(data, item => {
+                    return item[this.config.id_field] == this.value;
+                });
+            },
+
+            appendOption(item) {
+                $(this.$el).append(`<option value="${item[this.config.id_field]}" selected="true">${item[this.config.text_field]}</option>`)
+            },
+
+            processQuery(params) {
+                let query = {};
+
+                query[this.config.search_param] = params.term;
+
+                for (let key in this.config.query_params) {
+                    if (!this.config.query_params.hasOwnProperty(key)) {
+                        continue
+                    }
+
+                    query[key] = this.config.query_params[key];
+                }
+
+                return query;
+            },
+
+            processResults(data) {
+                let response = _.cloneDeep(data);
+
+                if (this.config.data_property) {
+                    response = response[this.config.data_property];
+                }
+
+                let results = _.map(response, item => {
+                    let disabled = typeof item.disabled !== 'undefined' ? item.disabled : false;
+                    let selected = typeof item.selected !== 'undefined' ? item.selected : false;
+
+                    return {
+                        id: item[this.config.id_field],
+                        text: item[this.config.text_field],
+                        selected: selected,
+                        disabled: disabled,
+                    }
+                });
+
+                return {
+                    results: results
+                }
+            }
+        },
+
         watch: {
             value: function (value) {
                 $(this.$el).val(value)
             },
             options: function (options) {
-                $(this.$el).select2({ data: options })
+                $(this.$el).select2({
+                    data: options
+                })
             }
         },
-        destroyed: () => {
+
+        destroyed() {
             $(this.$el).off().select2('destroy');
         }
     }
--- src/Base/Fields/Select2.php
+++ src/Base/Fields/Select2.php
@@ -7,9 +7,105 @@ use Laradium\Laradium\Base\Field;
 class Select2 extends Field
 {
     /**
+     * @var string|null
+     */
+    protected $source;
+
+    /**
+     * @var string
+     */
+    protected $dataProperty = 'data';
+
+    /**
+     * @var string
+     */
+    protected $searchParam = 'query';
+
+    /**
+     * @var array
+     */
+    protected $queryParams = [];
+
+    /**
+     * @var string
+     */
+    protected $idField = 'id';
+
+    /**
+     * @var string
+     */
+    protected $textField = 'text';
+
+    /**
      * @var array
      */
     protected $options = [];
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function source($value): self
+    {
+        $this->source = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function dataProperty($value): self
+    {
+        $this->dataProperty = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function searchParam($value): self
+    {
+        $this->searchParam = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function queryParams($value): self
+    {
+        $this->queryParams = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function idField($value): self
+    {
+        $this->idField = $value;
+
+        return $this;
+    }
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function textField($value): self
+    {
+        $this->textField = $value;
+
+        return $this;
+    }
 
     /**
      * @param array $options
@@ -29,6 +125,12 @@ class Select2 extends Field
     {
         $data = parent::formattedResponse();
         $data['options'] = $this->getOptions();
+        $data['config']['source'] = $this->source;
+        $data['config']['data_property'] = $this->dataProperty;
+        $data['config']['search_param'] = $this->searchParam;
+        $data['config']['query_params'] = $this->queryParams;
+        $data['config']['id_field'] = $this->idField;
+        $data['config']['text_field'] = $this->textField;
 
         return $data;
     }
@@ -36,7 +138,7 @@ class Select2 extends Field
     /**
      * @return array
      */
-    public function getOptions()
+    public function getOptions(): array
     {
         $options = [];
 
