c76ae9d810f97c07c1373419f5b3231f3ac4ad44
Ieva Zemņicka <ieva.zemnicka@netcore.lv>
2019-10-17 11:11:58 +0300

    Rename migration class name.

--- database/migrations/2019_10_17_123246_add_data_attributes_field_to_menu_items_table.php
+++ database/migrations/2019_10_17_123246_add_data_attributes_field_to_menu_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMenuItemsTable extends Migration
+class AddDataAttributesFieldToMenuItemsTable extends Migration
 {
     /**
      * Run the migrations.
