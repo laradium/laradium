0dfad90efb706439c71b74ad243a4c4f17bc1fc4
Ieva Zemņicka <ieva.zemnicka@netcore.lv>
2019-11-06 14:41:58 +0200

    Add a fix for renaming a table which contains enum type columns.

--- database/migrations/2018_12_02_114637_alter_nestedset_for_menu_items_table_rename_fields.php
+++ database/migrations/2018_12_02_114637_alter_nestedset_for_menu_items_table_rename_fields.php
@@ -3,10 +3,18 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Laradium\Laradium\Models\Menu;
+use Illuminate\Support\Facades\DB;
 
 class AlterNestedsetForMenuItemsTableRenameFields extends Migration
 {
+    /**
+     * AlterNestedsetForMenuItemsTableRenameFields constructor.
+     */
+    public function __construct()
+    {
+        DB::getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+    }
+
     /**
      * Run the migrations.
      *
